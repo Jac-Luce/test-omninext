bfc1a772eb308182f2395a265ab2f00d3d7ed763
Jac-Luce <luce.jac92@gmail.com>
2025-02-01 08:13:27 +0100

    modifiche card mobile

--- src/components/card/PianoGo.jsx
+++ src/components/card/PianoGo.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Button, OverlayTrigger, Tooltip, ListGroup, Container } from "react-bootstrap";
+import { Card, Button, OverlayTrigger, Tooltip, ListGroup, Container, Dropdown } from "react-bootstrap";
 import { CiCircleInfo } from "react-icons/ci";
 import { FaCheckCircle } from "react-icons/fa";
 import './pianoGo.css';
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
 
 export default function PianoGoCard() {
+    const [visibleList, setVisibleList] = useState(false);
+
+    const toggleList = () => { 
+        setVisibleList(!visibleList);
+    };
 
     return (
         <Container className="promoCard">
@@ -25,7 +32,7 @@ export default function PianoGoCard() {
                             </span>
                         </p>
                         <div className="divided"></div>
-                        <ListGroup>
+                        <ListGroup className="d-none d-md-block">
                             <ListGroup.Item className="border-0" style={{backgroundColor:'#E6ECF9'}}><FaCheckCircle /> Utenti illimitati</ListGroup.Item>
                             <ListGroup.Item className="border-0" style={{backgroundColor:'#E6ECF9'}}><FaCheckCircle /> Piattaforma MySarma</ListGroup.Item>
                             <ListGroup.Item className="border-0" style={{backgroundColor:'#E6ECF9'}}><FaCheckCircle /> Gestione del contratto online</ListGroup.Item>
@@ -33,6 +40,21 @@ export default function PianoGoCard() {
                             <ListGroup.Item className="border-0" style={{backgroundColor:'#E6ECF9'}}><FaCheckCircle /> Importi personalizzabili online</ListGroup.Item>
                             <ListGroup.Item className="border-0" style={{backgroundColor:'#E6ECF9'}}><FaCheckCircle /> Sconti e Coupon</ListGroup.Item>
                         </ListGroup>
+                        <>
+                            <Button className="d-block d-md-none btnMobileCard" variant="transparent" onClick={toggleList}>
+                                {visibleList ? <IoIosArrowUp /> : <IoIosArrowDown />}
+                            </Button>
+                            {visibleList && 
+                            <ListGroup>
+                                <ListGroup.Item className="border-0" style={{backgroundColor:'#E6ECF9'}}><FaCheckCircle /> Utenti illimitati</ListGroup.Item>
+                                <ListGroup.Item className="border-0" style={{backgroundColor:'#E6ECF9'}}><FaCheckCircle /> Piattaforma MySarma</ListGroup.Item>
+                                <ListGroup.Item className="border-0" style={{backgroundColor:'#E6ECF9'}}><FaCheckCircle /> Gestione del contratto online</ListGroup.Item>
+                                <ListGroup.Item className="border-0" style={{backgroundColor:'#E6ECF9'}}><FaCheckCircle /> MySarma Welfare Card (fino a 258€)</ListGroup.Item>
+                                <ListGroup.Item className="border-0" style={{backgroundColor:'#E6ECF9'}}><FaCheckCircle /> Importi personalizzabili online</ListGroup.Item>
+                                <ListGroup.Item className="border-0" style={{backgroundColor:'#E6ECF9'}}><FaCheckCircle /> Sconti e Coupon</ListGroup.Item>
+                            </ListGroup>
+                            }
+                        </>
                     </Card.Text>
                     <Button variant="rounded btnAttivaPiano px-4">Attiva Piano GO!</Button>
                     <div className="borderBtnGo"></div>
